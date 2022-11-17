cae4a39344c5b0203f110b3edb8c2e941f3ba5fc
Alana <59579972+aldoughty@users.noreply.github.com>
2022-11-17 08:05:15 -0600

    Add files via upload

--- Interface/IEnvironment.cs
+++ Interface/IEnvironment.cs
@@ -1,1 +1,13 @@
-
+﻿
+namespace ci_automation_apitester.Interface
+{
+    public interface IEnvironment
+    {
+        string GetBaseUrl(string environment);
+        string GetAuthenticationToken(string environment);
+        string GetApiKey(string environment);
+        List<string> GetAuthTypes();
+        string GetCurrentAuth();
+        public string GetAuthentication(string environment);
+    }
+}
---
+++ Interface/IRestApi.cs
@@ -0,0 +1,25 @@
+﻿namespace ci_automation_apitester.Interface
+{
+    public interface IRestApi
+    {
+        object SetTestParams(string authValue, IEnvironment environment);
+        List<RequestAttributeTest> GetRequestAttributeTests();
+        List<UrlAttributeTest> GetUrlAttributeTests();
+        string GetUrl(string action);
+        List<TestObjects.TestStep> GetParameterTests(MessageData messageData, string authValue, IEnvironment environment);
+        string GetPostBody();
+        string GetPutBody();
+        string GetPatchBody();
+        string GetDeleteBody();
+        string GetWorkingId();
+        string GetWorkingBody();
+        string GetTestType();
+        Dictionary<string, string> GetHeaders(string authValue, IEnvironment environment);
+        List<string> GetRequestTypes();
+        object GetResults(MessageData messageData, Dictionary<string, string> parameters);
+        void CleanUp(MessageData messageData, string currentId);
+        List<TestObjects.TestStep> GetFreeFormTests(string authValue, IEnvironment environment);
+        string GetRequestBodyByAction(string action);
+        Dictionary<string, string> GetHeaderParameter();
+    }
+}
